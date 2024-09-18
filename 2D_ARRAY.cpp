41592751781c025762c22aba864e5bf094173e4a
Siddarth <siddarth3055@gmail.com>
2024-09-18 11:49:31 +0530

    Create 2D_ARRAY.cpp

---
+++ 2D_ARRAY.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+
+    int array[rows][cols];
+
+    cout << "Enter the elements of the array:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cin >> array[i][j];
+        }
+    }
+
+    cout << "The array is:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
